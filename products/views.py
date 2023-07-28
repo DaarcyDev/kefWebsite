380f0aaf6a7468f87a3a0349826775dad82ced20
DaarcyDev <liam_alvarez@hotmail.com>
2023-07-27 22:59:26 -0600

    fixing securitty v0.9.3

--- products/views.py
+++ products/views.py
@@ -5,9 +5,13 @@ from django.contrib.auth import login, logout, authenticate
 from django.contrib.auth.models import User
 from .forms import productForm
 from .models import Product
-
+from django.contrib.auth.decorators import user_passes_test, login_required
 
 # Create your views here.
+def user_is_specific_user(user):
+    return user.username == 'daarcydev'  # Cambia 'nombre_de_usuario_especifico' por el nombre de usuario que deseas permitir
+
+
 def index(request):
     products = Product.objects.all()
     return render(request, 'index.html',{
@@ -55,6 +59,7 @@ def signup(request):
                 "error": "password dont match"
             })
 
+@user_passes_test(user_is_specific_user, login_url='index')
 def createProduct(request):
     if(request.method == "GET"):
         return render(request, 'createProduct.html',{
@@ -73,13 +78,18 @@ def createProduct(request):
             'error': 'please provide valide data'
         })
 
+@login_required
 def signout (request):
     logout(request)
     return redirect("index")
 
-def product(request):
-    return render(request, 'product.html')
+def product(request, product_id):
+    product = get_object_or_404(Product, pk=product_id)
+    return render(request, 'product.html', {
+        "product": product
+    })
 
+@user_passes_test(user_is_specific_user, login_url='index')
 def adminProduct(request, product_id):
     try:
         products = get_object_or_404(Product, pk=product_id, user=request.user)
@@ -89,12 +99,16 @@ def adminProduct(request, product_id):
     except:
         return redirect("admin")
 
+
+@user_passes_test(user_is_specific_user, login_url='index')
 def indexAdmin(request):
     products = Product.objects.all()
     return render(request, 'admin.html',{
         "products" : products
     })
 
+    
+@user_passes_test(user_is_specific_user, login_url='index')
 def updateProduct(request, product_id):
     if request.method == "GET":
         try:
@@ -118,6 +132,9 @@ def updateProduct(request, product_id):
             'form': productForm,
             'error': 'please provide valide data'
         })
+
+
+@user_passes_test(user_is_specific_user, login_url='index')
 def deleteProduct(request, product_id):
     products = get_object_or_404(Product, pk=product_id, user=request.user)
     if request.method == "POST":
